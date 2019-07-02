92624fbef01b4bd84c7a97ea7cc6b98d8c1835e7
Pavel <pav2089@gmail.com>
2019-07-02 22:19:18 +0300

    main process - completed

--- README.md
+++ README.md
@@ -3,13 +3,12 @@
 Процесс происходит в несколько этапов:
 1. Перекодирование видео дорожки в H.264 (под DASH формат) [ffmpeg]
 2. Перекодирование аудио дорожки в ACC (под DASH формат) [ffmpeg]
-3. Сведение обеих дорожек в один mp4 контейнер  [MP4Box]
-4. Разбиение на файла на блоки и создание манифеста. [MP4Box]
+3. Сведение обеих дорожек в mp4 контейнеры  [MP4Box]
+4. Разбиение файлов на блоки и создание манифеста. [MP4Box]
 
 Сейчас при работе с видео частью, комп кушает +5Gb оперативки.
 
 Планы на будущее:
-- Доделать шаги 3 и 4.
 - Возможно сделать шаги 1 и 2 в двух потоках (если не сожрет всю оперативку)
 - Добавить Jetty/Tomcat для вывода веб формы добавления видео файлов
 - Добавить JDBC для хранения информации о видео и манифест файлах
@@ -20,3 +19,18 @@
 https://github.com/bramp/ffmpeg-cli-wrapper
 http://itmultimedia.ru/sozdanie-kontenta-mpeg-dash-s-pomoshhyu-mp4box-i-ffmpeg/
 https://ru.smedialink.com/razrabotka/ffmpeg-i-ego-vrapper-dlya-java/
+
+
+Шпаргалка по командам для терминала:
+ffmpeg -i input.mp4 -vcodec libx264 -preset slow -x264opts fps=24:bitrate=2000:pass=1:vbv-maxrate=4000:vbv-bufsize=9000:keyint=96:min-keyint=96:scenecut=0:no-scenecut -vf scale=1280:720 input_video_b2000k.264
+
+ffmpeg -i input.mp4 -acodec libfdk_aac -b:a 128000 input_audio_b128k.aac
+
+MP4Box -add input_video_b2000k.264 input_video.mp4
+MP4Box -add input_audio_b128k.aac input_audio.mp4
+
+MP4Box -dash 4000 -frag 4000 -rap -segment-name %s/seg_ -url-template -out final.mpd input_video.mp4 input_audio.mp4
+
+кодек libfdk_aac не обязателен, можно использовать простой acc который уже идет в стандартной сборке ffmpeg.
+файл весом 162Мб - 25:30 после конвертации стал весить 410 МБ
+dash.js хорошо работает с получившемся файлом
--- src/main/java/ru/mybanana/VideoTranscoder.java
+++ src/main/java/ru/mybanana/VideoTranscoder.java
@@ -9,8 +9,10 @@ import net.bramp.ffmpeg.probe.FFmpegProbeResult;
 import net.bramp.ffmpeg.progress.Progress;
 import net.bramp.ffmpeg.progress.ProgressListener;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.File;
+import java.io.InputStreamReader;
 import java.util.concurrent.TimeUnit;
 
 public class VideoTranscoder {
@@ -31,10 +33,13 @@ public class VideoTranscoder {
 
             InputFileProbeResult = ffprobe.probe(InputFile.getAbsolutePath());
 
-            //File VideoOut = TransformVideo(InputFile);
+            File VideoOut = TransformVideo(InputFile);
             File AudioOut = TransformAudio(InputFile);
-            //File Mp4Packeg = PackingToMp4(VideoOut, AudioOut);
-            //Mp4ToDASH(Mp4Packeg);
+            //File VideoOut = new File("in/input_video_b2000k.264");
+            //File AudioOut = new File("in/input_audio_b128k.aac");
+            File Mp4VideoPackeg = PackingToMp4(VideoOut, "video");
+            File Mp4AudioPackeg = PackingToMp4(AudioOut, "audio");
+            Mp4ToDASH(Mp4VideoPackeg, Mp4AudioPackeg);
         } catch (IOException e){
             e.printStackTrace();
         }
@@ -123,12 +128,60 @@ public class VideoTranscoder {
         return new File("out/output.aac");
     }
 
-    private static File PackingToMp4(File VideoThrow, File AudioThrow){
-        //..
-        return new File("out/output.mp4");
+    private static File PackingToMp4(File ContentThrow, String type){
+        String OutputFile = "out/out"+type+".mp4";
+
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("bash", "-c", "MP4Box -add "+ContentThrow.getAbsolutePath()+" "+OutputFile);
+        try {
+
+            Process process = processBuilder.start();
+
+            StringBuilder output = new StringBuilder();
+
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(process.getInputStream()));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new File(OutputFile);
     }
 
-    private static void Mp4ToDASH(File Mp4Packeg){
-        System.out.println("Manifest complete");
+    private static void Mp4ToDASH(File Mp4VideoContainer,File Mp4AudioContainer){
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("bash", "-c", "MP4Box -dash 4000 -frag 4000 -rap -segment-name %s/seg_ -url-template -out out/final.mpd "+Mp4VideoContainer.getAbsolutePath()+" "+Mp4AudioContainer.getAbsolutePath());
+        try {
+
+            Process process = processBuilder.start();
+
+            StringBuilder output = new StringBuilder();
+
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(process.getInputStream()));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line + "\n");
+            }
+
+            int exitVal = process.waitFor();
+            if (exitVal == 0) {
+                System.out.println("Manifest complete");
+                System.out.println(output);
+                System.exit(0);
+            } else {
+                //abnormal...
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
