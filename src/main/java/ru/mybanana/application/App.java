46e462be48c4072a59cf424f1ab841c5081ecdb9
Pavel <pav2089@gmail.com>
2019-07-14 23:17:45 +0300

    start make uploading files

--- src/main/java/ru/mybanana/application/App.java
+++ src/main/java/ru/mybanana/application/App.java
@@ -1,7 +1,10 @@
 package ru.mybanana.application;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import ru.mybanana.application.service.StorageService;
 import ru.mybanana.media.VideoTranscoder;
 
 import java.io.IOException;
@@ -17,6 +20,14 @@ public class App {
 //            e.printStackTrace();
 //        }
         SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
     }
 
 }
--- src/main/java/ru/mybanana/application/controller/UploadController.java
+++ src/main/java/ru/mybanana/application/controller/UploadController.java
@@ -1,17 +1,37 @@
 package ru.mybanana.application.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import ru.mybanana.application.service.StorageService;
 
 @Controller
 public class UploadController {
 
+    private final StorageService storageService;
+
+    @Autowired
+    public UploadController(StorageService storageService){
+        this.storageService = storageService;
+    }
+
     @GetMapping("/upload/")
-    public String home(@RequestParam(name="file", required = false, defaultValue = "") String file, Model model){
+    public String uploadForm(@RequestParam(name="file", required = false, defaultValue = "") String file, Model model){
         model.addAttribute("file", file);
         return "upload";
+    }
+
+    @PostMapping("/upload/")
+    public String handleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes){
+        storageService.store(file);
+
+        redirectAttributes.addFlashAttribute("message", "Uploaded "+file.getOriginalFilename()+" - seccess");
+        return "redirect:/";
     }
 
 }
